46f6ed15d18863e48e5e4ce85407762a6fa0a712
alowe4 <117127121+alowe4@users.noreply.github.com>
2023-10-10 20:14:29 -0700

    Create Assignment2.ps1

---
+++ Assignment2/Assignment2.ps1
@@ -0,0 +1,7 @@
+$SubscriptionId = "42f90695-f39c-451f-8b08-dc2ddfc9c182" 
+$ResourceGroup = "myResourceGroup" 
+$WebAppName = "MyFirstAzureWebAppCSCD396-fall2023" 
+$WebAppUrl = "myfirstazurewebappcscd396-fall2023.azurewebsites.net" 
+$KeyVault = "cscd369fallkeyvault" 
+$SecretName = "" 
+$StorageAccount = "secureappstoragecscd369"
