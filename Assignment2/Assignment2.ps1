9b50ab90db63048b82cf5e39bfdaf35b3b5d1ee4
TheMadMystic <65517955+TheMadMystic@users.noreply.github.com>
2023-10-12 21:50:01 -0700

    Assignment2 Complete

---
+++ Assignment2/Assignment2.ps1
@@ -0,0 +1,7 @@
+$SubscriptionId = "70c2f543-67b0-47d9-ab82-72a1e06fa0bc"
+$ResourceGroup = "Assignment2"
+$WebAppName = "FirstAzureWebApp-awallace8"
+$WebAppUrl = "firstazurewebapp-awallace8.azurewebsites.net"
+$KeyVault = "WebAppKeyVaultawallace8"
+$SecretName = "SecretNumber"
+$StorageAccount = "webappstorageawallace8"
