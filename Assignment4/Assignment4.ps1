bd87cf433ace52ef339b71278e719931c34b4e23
Zrigg <74619022+Zrigg@users.noreply.github.com>
2023-11-09 19:18:02 -0800

    Create Assignment4.ps1

---
+++ Assignment4/Assignment4.ps1
@@ -0,0 +1,8 @@
+$SubscriptionId = "df29ccc0-1d4d-42b8-987d-c1f04f575600"
+$ResourceGroup = "MyResourceGroup"
+$LogAnalyticsWorkspaceName = "MyWorkspace"
+$LogAnalyticsTableName = "MyTable_CL"
+$DataCollectionRuleName = ""
+$DataCollectionEndpointName = "ZriggDCE"
+$FunctionAppName = "ZriggCSCD396FunctionApp"
+$ApiManagementGatewayName = ""
